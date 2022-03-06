2bec2ad909b865e31d192b2a27ef650cb3093545
gundamking <34838832+gundamking@users.noreply.github.com>
2022-03-06 14:36:01 -0500

    data for script

--- germancreditml.py
+++ germancreditml.py
@@ -6,44 +6,11 @@ sns.set_style('whitegrid')
 
 filename = 'german.data'
 credit_df = pd.read_csv( "german.data", delim_whitespace = True, header = None)
-# columns = ['checkin_acc', 'duration', 'credit_history', 'purpose', 'amount',
-#            'saving_acc', 'present_emp_since', 'inst_rate', 'personal_status',
-#            'other_debtors', 'residing_since', 'property', 'age',
-#            'inst_plans', 'housing', 'num_credits',
-#            'job', 'dependents', 'telephone', 'foreign_worker', 'status']
-#
-# credit_df.columns = columns
-# credit_df.target_names = ['Good', 'Bad']
-# X_features = list( credit_df.columns )
-# credit_df_complete = pd.get_dummies( credit_df[X_features], drop_first = True )
-# print(credit_df.info())
-# print(X_features)
-# credit_df_complete = pd.get_dummies( credit_df[X_features], drop_first = True )
-# print(credit_df_complete.head())
-#
-# import seaborn as sns
-# import matplotlib
-# import matplotlib.pyplot as plt
-#
-# sns.set_style('darkgrid')
-# matplotlib.rcParams['font.size'] = 14
-# matplotlib.rcParams['figure.figsize'] = (9, 5)
-# matplotlib.rcParams['figure.facecolor'] = '#00000000'
-# print(credit_df_complete.isna().any().any())
-# credit_df_complete.status.unique()
-#
-#
-# from sklearn.preprocessing import LabelEncoder
-#
-# le = LabelEncoder()
-# le.fit(credit_df_complete.status)
-# credit_df_complete = le.transform(credit_df_complete.status)
-# print(credit_df_complete.status.head(5))
 
-credit_df.info()
+#credit_df.info()
 credit_df.columns=['account_bal','duration','payment_status','purpose',
                    'credit_amount','savings_bond_value','employed_since',
-                   'intallment_rate','sex_marital','guarantor','residence_since',
+                   'installment_rate','sex_marital','guarantor','residence_since',
                    'most_valuable_asset','age','concurrent_credits','type_of_housing',
                    'number_of_existcr','job','number_of_dependents','telephone',
                    'foreign','target']
@@ -76,14 +43,99 @@ credit_df.target=le.transform(credit_df.target)
 credit_df.target.head(5)
 
 good_bad_per=round(((credit_df.target.value_counts()/credit_df.target.count())*100))
-good_bad_per
-print(plt.pie(good_bad_per,labels=['Good loans', 'Bad loans'], autopct='%1.0f%%', startangle=90))
-plt.title('Percentage of good and bad loans')
+#print(good_bad_per)
+#plt.pie(good_bad_per,labels=['Good loans', 'Bad loans'], autopct='%1.0f%%', startangle=90)
+#plt.title('Percentage of good and bad loans')
+credit_df['credit_amount']=np.log(credit_df['credit_amount'])
+
+#print(credit_df[['credit_amount','duration','age']].describe())
+
+# #fig, axes = plt.subplots(1,3, figsize=(16,8))
+# plt.suptitle('Histogram of continuous variables')
+# axes[0].hist(credit_df['duration'])
+# axes[0].set_xlabel('No. of observations')
+# axes[0].set_ylabel('Years')
+# axes[0].set_title('Histogram of loan duration');
+#
+# axes[1].hist(credit_df['credit_amount'])
+# axes[1].set_xlabel('No. of observations')
+# axes[1].set_ylabel('Credit amount (dollars)')
+# axes[1].set_title('Histogram of Credit amount');
+#
+# axes[2].hist(credit_df['age'])
+# axes[2].set_xlabel('No. of observations')
+# axes[2].set_ylabel('Age')
+# axes[2].set_title('Histogram of Age');
+#
+# #plt.show()
+#
+# # box-plots of continues variables
+#
+# fig, ax = plt.subplots(1,3,figsize=(20,5))
+# plt.suptitle('BOX PLOTS')
+# sns.boxplot(credit_df['credit_amount'], ax=ax[0]);
+# sns.boxplot(credit_df['duration'], ax=ax[1], color='salmon');
+# sns.boxplot(credit_df['age'], ax=ax[2], color='darkviolet');
+#plt.show()
+
+#sns.scatterplot(y=credit_df.credit_amount,x=credit_df.duration, hue=credit_df.target, s=100)
+#plt.show()
+
+#credit_df.groupby('job')['target'].value_counts().unstack(level=1).plot.barh(stacked=True, figsize=(10, 6))
+
+#sns.lineplot(data=credit_df, x='duration', y='credit_amount', hue='target', palette='deep');
+
+#credit_df.groupby('most_valuable_asset')['target'].value_counts().unstack(level=1).plot.barh(stacked=True, figsize=(10, 6))
+
+#sns.scatterplot(y=credit_df.credit_amount, x=credit_df.most_valuable_asset, hue=credit_df.target, s=100);
+#plt.show()
+
+# Number of unique classes in each object column
+print(credit_df.select_dtypes('object').apply(pd.Series.nunique, axis = 0))
+
+# sklearn preprocessing for dealing with categorical variables
+from sklearn.preprocessing import LabelEncoder
+
+# Create a label encoder object
+le = LabelEncoder()
+le_count = 0
+
+# Iterate through the columns
+for col in credit_df:
+    if credit_df[col].dtype == 'object':
+        # If 2 or fewer unique categories
+        if len(list(credit_df[col].unique())) <= 2:
+            # Train on the training data
+            le.fit(credit_df[col])
+            # Transform both training and testing data
+            credit_df[col] = le.transform(credit_df[col])
+
+            # Keep track of how many columns were label encoded
+            le_count += 1
+
+#print('%d columns were label encoded.' % le_count)
+
+credit_df = pd.get_dummies(credit_df)
+
+#print('Encoded Features shape: ', credit_df.shape)
+
+# Find correlations with the target and sort
+correlations = credit_df.corr()['target'].sort_values()
+
+# Display correlations
+print('Most Positive Correlations:\n', correlations.tail(15))
+print('\nMost Negative Correlations:\n', correlations.head(15))
+
+# Extract the significantly correlated variables
+#corr_data = credit_df[['duration','credit_amount',
+                   'installment_rate','residence_since',
+                   'age','number_of_existcr','number_of_dependents','telephone',
+                   'foreign','target']]
+corr_data_corrs = corr_data.corr()
 
 
-
-
-
-
-
+# Heatmap of correlations
+sns.heatmap(corr_data_corrs, cmap = plt.cm.RdYlBu_r, vmin = -0.25, annot = True, vmax = 0.6)
+plt.title('Correlation Heatmap');
+plt.show()
 
